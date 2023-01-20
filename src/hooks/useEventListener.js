38bd2e22cebfe5be1950dd6fe77a2478a193166e
antonkarytski <heyheyjude23@gmail.com>
2023-01-20 14:14:00 +0100

    Updates

--- src/hooks/useEventListener.js
+++
@@ -1,8 +0,0 @@
-import {useEffect} from 'react'
-
-export default function useEventListener(fn, action){
-    useEffect(() => {
-        window.addEventListener(action, fn);
-        return () => window.removeEventListener(action, fn);
-    },[fn, action])
-}
